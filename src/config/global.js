38ec0a1a8eeb50e96c79923c7c035e37c2426685
jhonuru <jhon.urueta@misena.edu.co>
2024-04-05 10:44:40 -0500

    Ajustes 6

--- src/config/global.js
+++ src/config/global.js
@@ -186,7 +186,7 @@ export default {
       {
         icono: 'fas fa-file-pdf',
         titulo: 'Descargar PDF',
-        download: 'downloads/prueba.pdf',
+        download: 'downloads/228128_CF06_DU.pdf',
       },
       {
         icono: 'fas fa-download',
--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -8,7 +8,7 @@
         span
           i.fas.fa-info
       h1 Introducción
-    .row
+    .row.align-items-center
       .col-lg-8.order-2.order-lg-1(data-aos="fade-right")
         p Le damos la bienvenida al componente formativo “Patrones de diseño de #[i software”]. Recordemos que los patrones de diseño surgen como un concepto, inicialmente, para el área de arquitectura e ingeniería civil, pues era en estas disciplinas donde se presentaban casos particulares que requerían de soluciones a problemáticas particulares.
         p El concepto de patrón de diseño se empezó a asociar al diseño orientado a objetos y, en lugar de referirse a la forma de colocar paredes, puertas o ventanas, se refería a la forma en que se construyen clases, objetos, interfaces, y a la forma en cómo estos deben interactuar.
@@ -21,11 +21,10 @@
                     img(src='@/assets/curso/intro/img1.svg', style="width:50px", alt='').m-auto
                 .col
                   p.mb-0 El uso de patrones de diseño permite acelerar el proceso de desarrollo, al proporcionar paradigmas probados y comprobados en el diseño de #[i software]. Estandarizar las soluciones a problemas comunes permite también mejorar la comunicación entre desarrolladores, ya que se emplean nombres bien conocidos y entendidos a la hora de describir un problema de diseño y cómo abordar su solución.
-        p A continuación, se abordan varios de los patrones de diseño más representativos en la industria del #[i software], los cuales nos permitirán, a futuro, implementar soluciones más robustas, de acuerdo con altos estándares de calidad y recomendaciones ampliamente conocidas en la industria, lo que adicionalmente nos dará un plus como desarrolladores de #[i software] en una industria cada vez más competitiva globalmente. 
-        p El uso de patrones de diseño permite acelerar el proceso de desarrollo, al proporcionar paradigmas probados y comprobados en el diseño de #[i software]. Estandarizar las soluciones a problemas comunes permite también mejorar la comunicación entre desarrolladores, ya que se emplean nombres bien conocidos y entendidos a la hora de describir un problema de diseño y cómo abordar su solución.
+        p.mb-0 A continuación, se abordan varios de los patrones de diseño más representativos en la industria del #[i software], los cuales nos permitirán, a futuro, implementar soluciones más robustas, de acuerdo con altos estándares de calidad y recomendaciones ampliamente conocidas en la industria, lo que adicionalmente nos dará un plus como desarrolladores de #[i software] en una industria cada vez más competitiva globalmente. 
       .col-lg-4.mb-4.mb-lg-0.order-1.order-lg-2(data-aos="fade-left")
         figure.mt-0.mt-lg-5
-          img(src='@/assets/curso/intro/img2.svg', style="width:450px", alt='').m-auto
+          img(src='@/assets/curso/intro/img2.svg', style="width:350px", alt='').m-auto
     
 </template>
 

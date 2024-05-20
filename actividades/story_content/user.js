3cc7497e63cfcbb1bf9b062ee4e6aa29d2c4c2bd
maoSantaella <mao.santaella@gmail.com>
2024-05-20 18:20:49 +0000

    new dist

---
+++ actividades/story_content/user.js
@@ -0,0 +1,23 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "5g6RTmaZst1":
+        Script1();
+        break;
+  }
+}
+
+window.InitExecuteScripts = function()
+{
+var player = GetPlayer();
+var object = player.object;
+var addToTimeline = player.addToTimeline;
+var setVar = player.SetVar;
+var getVar = player.GetVar;
+window.Script1 = function()
+{
+  window.close();
+}
+
+};
